0ba4e026e137a5902ac73ce0fd09f865c77abf40
Shambhavi Rai <112109952+luna1902@users.noreply.github.com>
2024-04-13 15:26:38 +0530

    pushing previous updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
-<script>
-    import "../app.css";
+<script lang="ts">
+   import { page } from '$app/stores';
+	
+	import { ProgressBar } from '@prgm/sveltekit-progress-bar';
+	import { ModeWatcher } from 'mode-watcher';
+	import { CodeSolid, HeartRegular } from 'svelte-awesome-icons';
+	import { MetaTags } from 'svelte-meta-tags';
+	import '../app.css';
+	$: location = $page.data.location;
+   import "../app.css";
   </script>
   
   <slot />
